f80446f2950bdee03be29e84a692bfa2d4bc3ba5
Mathieu BOREL <mathieu.borel@epitech.eu>
2022-12-02 16:10:05 +0100

    add abricot

---
+++ abricot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$EUID" -eq 0 -o "$UID" -eq 0 ]
+	then
+		sudo dnf install python3
+		sudo dnf install python-pip
+		python3 -m pip install pcpp
+		curl https://raw.githubusercontent.com/Just1truc/Abricot-Norminette/main/get_abricot.sh | sh
+		exit
+else
+	echo Must be sudo
+	exit
+fi
